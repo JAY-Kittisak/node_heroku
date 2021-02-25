ef14140067e3bc95290bba2036b7f5c559fcbda5
JAY WFH <jay46789@gmail.com>
2021-02-25 22:16:32 +0700

    stst

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import createServer from "./createServer";
 
 const startServer = () => {
   const port = process.env.PORT || 3000;
   const app = express();
+  const server = createServer();
+
+  server.applyMiddleware({ app });
 
   app.listen(port, () =>
-    console.log(`Server is  ready at http://localhost:${port}`)
+    console.log(
+      `Server is  ready at http://localhost:${port}${server.graphqlPath}`
+    )
   );
 };
 
