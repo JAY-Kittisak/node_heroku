86359fbc619b2b6a260a907bd5880767d626305d
JAY WFH <jay46789@gmail.com>
2021-02-25 21:47:16 +0700

    stst

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import express from "express";
 const startServer = () => {
   const port = process.env.PORT || 3000;
   const app = express();
-  app.get("/", (req, res) => {
-    res.send("Hello World");
-  });
 
   app.listen(port, () =>
     console.log(`Server is  ready at http://localhost:${port}`)
