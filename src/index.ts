c3492756853320993024c908fde2288a6602e59f
JAY WFH <jay46789@gmail.com>
2021-02-26 19:32:44 +0700

    'Heroku'

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const { PORT_DEV, DB_USER, DB_PASSWORD, DB_ENDPOINT, DB_NAME } = process.env;
 
 const startServer = async () => {
     try {
-        // Connect to the database
+        // Connect to the database *_*
         await mongoose.connect(
             `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_ENDPOINT}/${DB_NAME}?retryWrites=true&w=majority`,
             {
